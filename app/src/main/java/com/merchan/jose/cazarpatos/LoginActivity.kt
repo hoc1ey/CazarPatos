de31bba504aacbc0b8da010f205403f7d82321da
José Emanuel Merchán Sánchez <jose.merchan01@epn.edu.ec>
2025-05-16 15:10:29 -0500

    code(master): added login and main screens.

--- app/src/main/java/com/merchan/jose/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/merchan/jose/cazarpatos/LoginActivity.kt
@@ -1,20 +1,39 @@
-package com.example.josemerchanejemplo1
+package com.merchan.jose.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity3 : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var buttonLoginButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initUIComponents()
+        initEventListeners()
+
+    }
+
+    private fun initEventListeners() {
+        buttonLoginButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
+    private fun initUIComponents() {
+        buttonLoginButton = findViewById(R.id.buttonLoginButton)
     }
 }
--- app/src/main/java/com/merchan/jose/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/merchan/jose/cazarpatos/MainActivity.kt
@@ -1,39 +1,20 @@
 package com.merchan.jose.cazarpatos
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var buttonLoginButton : Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        initUIComponents()
-        initEventListeners()
-
-    }
-
-    private fun initEventListeners() {
-        buttonLoginButton.setOnClickListener {
-            startActivity(Intent(this, MainActivity2::class.java))
-        }
-    }
-
-    private fun initUIComponents() {
-        buttonLoginButton = findViewById(R.id.buttonLoginButton)
     }
 }
